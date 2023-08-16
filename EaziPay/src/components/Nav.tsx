30da0bf06e3f9476fe0534cc088ca9fcc77f9ca4
Gloria <ubahgloria16@gmail.com>
2023-08-16 17:27:36 +0100

    Setup done

---
+++ EaziPay/src/components/Nav.tsx
@@ -0,0 +1,12 @@
+import Logo from "../Assets/EAZIPAY LOGO NEW 1.png";
+const Nav = () => {
+     return (
+          <nav>
+               <div>
+               <img src={Logo} alt="" />
+               </div>
+          </nav>
+     )
+};
+
+export default Nav;
