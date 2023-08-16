39e34b38b6f67b263ff30280d9cfc3321630da41
Gloria <ubahgloria16@gmail.com>
2023-08-16 17:28:45 +0100

    Setup

---
+++ EaziPay/src/App.tsx
@@ -0,0 +1,70 @@
+import { useState, useEffect, createContext, Dispatch, SetStateAction } from "react";
+import { createHashRouter, RouterProvider, Outlet } from "react-router-dom";
+import axios from "axios";
+import LandingPage from "./pages/landingPage";
+
+
+export type UserState = {
+  setUsers: Dispatch<SetStateAction<never[]>>
+};
+const setState: UserState = {
+  setUsers: state => state
+};
+export const UserContext = createContext({ users: [], setUsers: setState.setUsers});
+export const ToggleContext = createContext(false);
+
+const Layout = () => {
+  const [toggle] = useState(false);
+  const [users, setUsers] = useState([]);
+  const lendsqrUsers = `https://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1/users`;
+  useEffect(() => {
+    axios
+      .get(lendsqrUsers)
+      .then((response) => {
+        setUsers(response.data);
+      })
+      .catch((err) => console.log(err));
+  }, [lendsqrUsers]);
+
+  return (
+    <UserContext.Provider value={{ users, setUsers }}>
+      <ToggleContext.Provider value={ toggle }>
+        {/* <Dashboard/> */}
+        <Outlet />
+      </ToggleContext.Provider>
+    </UserContext.Provider>
+  )
+}
+
+function App() {
+  const router = createHashRouter(
+    [
+      {
+        path: "/",
+        element: <LandingPage />
+      },
+      {
+        path: "/dashboard",
+        element: <Layout />,
+        children: [
+          // {
+          //   index: true,
+          //   element: <Users />
+          // },
+          // {
+          //   path: "user-page",
+          //   element: <UserPage />
+          // },
+        ]
+      }
+    ]
+  )
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
---
+++ EaziPay/src/pages/landingPage.tsx
@@ -0,0 +1,11 @@
+import Nav from "../components/Nav";
+
+const LandingPage = () => {
+     return(
+          <section className={``}>
+               <Nav />
+          </section>
+     )
+}
+
+export default LandingPage;
